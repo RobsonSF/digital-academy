e28014a5fb3057a84961682d124d19881c6b1eaf
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-04 14:56:00 -0300

    feat: seeding H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Robson', 'rbs@rbs.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Luana', 'luana@luana.com', '654321');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@maria.com', '654321');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
