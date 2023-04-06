35edd1efa639ccaa54c272c33e34c5c0217ddd56
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-06 18:48:59 -0300

    feat: Enrollment created

---
+++ src/main/java/com/fernandes/digitalacademy/entities/Enrollment.java
@@ -0,0 +1,98 @@
+package com.fernandes.digitalacademy.entities;
+
+import com.fernandes.digitalacademy.entities.pk.EnrollmentId;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID =  1L;
+
+    @EmbeddedId
+    private EnrollmentId id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant  refundMoment = null;
+    private  boolean available = true;
+    private boolean onlyUpdate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(
+            User user,
+            Offer offer,
+            Instant enrollMoment,
+            Instant refundMoment,
+            boolean available,
+            boolean onlyUpdate
+    ) {
+        this.id = new EnrollmentId(user, offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setStudent(Offer offer) {
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+
+}
