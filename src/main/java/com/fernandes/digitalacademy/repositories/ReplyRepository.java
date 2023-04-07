9580161b73f74330b98ed4a87e381bdbe07a98db
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-07 15:25:23 -0300

    feat: ReplyRepository created

---
+++ src/main/java/com/fernandes/digitalacademy/repositories/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.fernandes.digitalacademy.repositories;
+
+import com.fernandes.digitalacademy.entities.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
