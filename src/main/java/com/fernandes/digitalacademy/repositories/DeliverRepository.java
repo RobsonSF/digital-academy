921af6247b0599411282ad875d5dcf193a6e138a
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-07 15:30:44 -0300

    feat: DeliverRepository created

---
+++ src/main/java/com/fernandes/digitalacademy/repositories/DeliverRepository.java
@@ -0,0 +1,7 @@
+package com.fernandes.digitalacademy.repositories;
+
+import com.fernandes.digitalacademy.entities.Deliver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliverRepository extends JpaRepository<Deliver, Long> {
+}
