59e360cd1a4eb2a3003cd055db4f5a0bf7b1e1fd
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-07 15:08:42 -0300

    feat: add topics

--- src/main/java/com/fernandes/digitalacademy/entities/Lesson.java
+++ src/main/java/com/fernandes/digitalacademy/entities/Lesson.java
@@ -37,6 +37,9 @@ public abstract class Lesson implements Serializable {
     @OneToMany(mappedBy = "lesson")
     private List<Deliver> deliveries =  new ArrayList<>();
 
+    @OneToMany(mappedBy = "lesson")
+    private List<Topic> topics = new ArrayList<>();
+
     public Lesson() {
     }
 
@@ -84,6 +87,10 @@ public abstract class Lesson implements Serializable {
 
     public List<Deliver> getDeliveries() {
         return deliveries;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
     }
 
     @Override
