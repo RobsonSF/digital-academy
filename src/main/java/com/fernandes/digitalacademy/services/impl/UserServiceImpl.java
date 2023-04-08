ec110a80c5e42e5ef1a9f6a2786c0073d0f9cd20
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-08 19:58:47 -0300

    feat: implemented findById and Update to UserServiceImpl

--- src/main/java/com/fernandes/digitalacademy/services/impl/UserServiceImpl.java
+++ src/main/java/com/fernandes/digitalacademy/services/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.fernandes.digitalacademy.entities.User;
 import com.fernandes.digitalacademy.model.request.UserRequest;
 import com.fernandes.digitalacademy.repositories.UserRepository;
 import com.fernandes.digitalacademy.services.UserService;
+import com.fernandes.digitalacademy.services.exceptions.ResourceNotFoundException;
+import com.fernandes.digitalacademy.services.mapper.UserRequestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,24 +17,34 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private UserRequestMapper mapper;
+
     @Override
     public Page<User> findAllPaged(Pageable pageable) {
         return repository.findAll(pageable);
     }
 
     @Override
-    public User findByID(Long id) {
-        return null;
+    public User findById(Long id) {
+        var optionalUser = repository.findById(id);
+        return optionalUser.orElseThrow(
+                () -> new ResourceNotFoundException("User not found")
+        );
     }
 
     @Override
     public User insert(UserRequest userRequest) {
-        return null;
+        var user = mapper.toUserEntity(userRequest);
+        return repository.save(user);
     }
 
     @Override
-    public User update(Long id, UserRequest UserRequest) {
-        return null;
+    public User update(Long id, UserRequest userRequest) {
+        var referenceId = repository.getReferenceById(id);
+        var user = mapper.toUserEntity(userRequest);
+        user.setId(referenceId.getId());
+        return repository.save(user);
     }
 
     @Override
