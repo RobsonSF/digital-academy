c266db59596c256de67287ead63a6008c35994c8
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-08 19:55:28 -0300

    refactor: imports correction

--- src/main/java/com/fernandes/digitalacademy/model/request/UserRequest.java
+++ src/main/java/com/fernandes/digitalacademy/model/request/UserRequest.java
@@ -1,21 +1,6 @@
 package com.fernandes.digitalacademy.model.request;
-
-//import jakarta.validation.constraints.Email;
-//import jakarta.validation.constraints.NotEmpty;
-//import jakarta.validation.constraints.Size;
-
 public record UserRequest(
-
-                //@Size(min =  3, max =50, message = "must be between {min} and {max} characters")
-                //@NotEmpty(message = "must not be null or empty")
-                String name,
-
-                //@Email(message = "invalid e-mail")
-                //@NotEmpty(message = "must not be null or empty")
-                String email,
-
-               // @Size(min =  6, max =10, message = "must be between {min} and {max} characters")
-               // @NotEmpty(message = "must not be null or empty")
-                String password
-) {
-}
+        String name,
+        String email,
+        String password
+) {}
