b100f0a3a5bfdd9971c00bf9d21a9b87850c6c92
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-08 13:08:58 -0300

    feat: UserService created

---
+++ src/main/java/com/fernandes/digitalacademy/services/UserService.java
@@ -0,0 +1,25 @@
+package com.fernandes.digitalacademy.services;
+
+import com.fernandes.digitalacademy.entities.User;
+import com.fernandes.digitalacademy.model.request.UserRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserService {
+
+    @Transactional(readOnly = true)
+    Page<User> findAllPaged(Pageable pageable);
+
+    @Transactional(readOnly = true)
+    User findByID(Long id);
+
+    @Transactional
+
+    User insert(UserRequest userRequest);
+
+    @Transactional
+    User update(Long id, UserRequest UserRequest);
+
+    void delete(Long id);
+}
